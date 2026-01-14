fe955859a7321cbece29c32d826d2cc53570601c
PhilippeItsMe <144894663+PhilippeItsMe@users.noreply.github.com>
2026-01-14 12:00:12 +0100

    dev - lightbox setup

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,21 @@
 const mediaQuery = window.matchMedia('(max-width: 900px)');
 
+// Menu const
 const menuDesktop = document.querySelector('#desktop-menu');
 const menuMobileClosed = document.querySelector('#mobile-menu-closed');
 const menuMobileClosedButton = document.querySelector('#mobile-menu-closed button');
 const menuMobileOpen = document.querySelector('#mobile-menu-open');
 const menuMobileOpenButton = document.querySelector('#mobile-menu-open button');
 
+// Main Illustration const
+const mainPicture = document.querySelector('.main-picture');
+const secondaryPictures = document.querySelector('.secondary-pictures');
+const secondaryPicturesAllImg = document.querySelectorAll('.secondary-pictures img')
+
+// Lightbox Illustration const
+const mainPictureLightbox = document.querySelector('.lightbox-content .main-picture');
+const secondaryPicturesLightbox = document.querySelector('.lightbox-content .secondary-pictures');
+const secondaryPicturesAllImgLightbox = document.querySelectorAll('.lightbox-content .secondary-pictures img')
 
 
 // ----------  Switch between desktop & mobile menu ---------- //
@@ -24,9 +34,9 @@ function handelMenu(e) {
     }  
 };
 
-//Execution when uploading the page
+// Execution when uploading the page
 handelMenu(mediaQuery);
-//Execution when page size changes
+// Execution when page size changes
 mediaQuery.addEventListener('change', handelMenu);
 
 
@@ -46,5 +56,42 @@ menuMobileOpenButton.addEventListener('click', (e) => {
     menuMobileOpen.style.display = 'none';
     menuMobileOpen.setAttribute('aria-hidden', 'true');
 });
+
+
+// ----------  Picture slider switch ---------- //
+
+//In the main
+secondaryPictures.addEventListener ('click', (e) => {
+    const button = e.target.closest('button');
+    const buttonImg = button.querySelector('img');
+    const buttonImgScr = buttonImg.src.replace('-thumbnail', '');
+    
+    secondaryPicturesAllImgLightbox.forEach((img) => {
+        img.style.opacity = '1';
+    });
+    
+    buttonImg.style.opacity = '0.5';
+    mainPicture.setAttribute('src', buttonImgScr);
+});
+
+
+//In the lightbox
+secondaryPicturesLightbox.addEventListener ('click', (e) => {
+    const button = e.target.closest('button');
+    const buttonImg = button.querySelector('img');
+    const buttonImgScr = buttonImg.src.replace('-thumbnail', '');
+    
+    secondaryPicturesAllImg.forEach((img) => {
+        img.style.opacity = '1';
+    });
+    
+    buttonImg.style.opacity = '0.5';
+    mainPictureLightbox.setAttribute('src', buttonImgScr);
+});
+
+
+
+
+
 
 
